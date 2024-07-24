bbc5f8f1e9c148ac7446ee5476745f5a96c3b17d
ManuelMannhold <M.Mannhold@gmx.de>
2024-07-24 20:53:47 +0200

    add images and positions

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,15 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 250;
     img;
+    width = 100;
+    height = 150;
+
+    //loadImage('img/test.png');
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
+        this.img.src = path;
+    }
 
      moveRight() {
         console.log('moving right');
