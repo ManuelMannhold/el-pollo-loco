73b91266b1f45a9c90c8db356d8e543731c5f448
ManuelMannhold <M.Mannhold@gmx.de>
2024-07-25 20:16:21 +0200

    add background and style

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,6 @@
 class Character extends MovableObject {
+    height = 230;
+    y = 200;
     
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,8 @@
 class Chicken extends MovableObject {
-   
+    width = 60;
+    height = 50;
+    y = 380;
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
